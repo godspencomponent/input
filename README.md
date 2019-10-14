3216c1c3a54df864bc8032a171b5ef86e2c10cd9
panaru <aru.pana@ymm56.com>
2019-10-14 15:25:03 +0800

    1.0.1 输入框 开发完成

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-写点组件使用说明吧
+## 组件：输入框
+### 配置项
+- 默认值（初始值）
+配置默认值有两种方案：①直接属性面板输入；②脚本方法写入`this.setValue(val)`
+- 占位符配置
+- 输入框类型：文本、数值、密码、复选框
+- 最大字符数（默认为-1，不限制）
+
+### 获取输入值，可以使用`getValue`方法
+
+#### demo 
+组件1：输入框，id为"input",脚本设置初始值
+```
+<!-- input.js -->
+
+// 设置初始值
+this.setValue('我是初始值')
+
+// 获取输入值
+this.getValue()
+```
+组件2：提交按钮，id为"btn",点击按钮提交数据
+```
+<!-- btn.js -->
+submit (id) {
+    let val
+    let $com = this.$parent.getComponent(id)
+    if (!!$com) val = $com.$refs[id].getValue()
+    console.log('输入值为', val)
+}
+```
--- src/index.vue
+++ src/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component">
-    hello world
+    <input class="component" :class="{'checkbox': type == 'checkbox' }" :type="type" :readonly='readonly' :maxlength='maxlength' :placeholder='placeholder' v-model='value'/>
   </div>
 </template>
 
@@ -13,18 +13,89 @@
     label: process.env.LABEL,
     style: process.env.STYLE,
     props: {
+      text: {
+        type: [String, Number],
+        default: '',
+        editer: {
+          type: 'text',
+          label: '默认值',
+        }
+      },
+      placeholder: {
+        type: [String, Number],
+        default: '请输入文本',
+        editer: {
+          type: 'text',
+          label: '占位符',
+          desc: '提示用户输入框的作用'
+        }
+      },
+      type: {
+        type: String,
+        default: 'text',
+        editer: {
+          label: '类型',
+          type: 'enum',
+          defaultList: {
+            text: '文本',
+            tel: '数值',
+            password: '密码',
+            checkbox: '复选框'
+          }
+        }
+      },
+      readonly: {
+        type: Boolean,
+        default: false,
+        editer: {
+          label: '只读'
+        }
+      },
+      maxlength: {
+        type: Number,
+        default: -1,
+        editer: {
+          label: '最大字符数'
+        }
+      }
+    },
+    data () {
+      return {
+        value: ''
+      }
+    },
+    watch: {
+      text () {
+        this.value = this.scopeGet('text')
+      }
     },
     computed: {
     },
-    editorMethods: {
+    editerMethods: {
     },
     methods: {
+      getValue () {
+        return this.value
+      },
+      setValue (val) {
+        this.value = val
+      }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
   .component {
-    color: red;
+    width: 100%;
+    height: 100%;
+    outline: none;
+    border: none;
+    background: none;
+    appearance none
+    &.checkbox {
+      width: auto;
+      height: auto;
+      appearance: checkbox;
+    }
   }
 </style>
